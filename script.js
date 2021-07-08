ebd047fc10dfeaf7e02e4fef63b0c29998dcc657
Amimul ihsan <86657812+amimul21@users.noreply.github.com>
2021-07-08 21:26:51 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 
-var inp1 = $("#input1").val();
-var inp2 = $("#input2").val();
+var inp1 = document.getElementById('input1').value;
+var inp2 = document.getElementById('input2').value;
 var minus = inp1-inp2;
 var plus = parseInt(inp1)+parseInt(inp2);
 var mult = inp1*inp2;
 var div = inp1/inp2;
 
 
-$("#result").text();
 
-$(".btn2").click(function(){
-    $("#result").text(inp1 + " - " + inp2 + " = " + minus);
-});
+function toPuls(){
+    document.getElementById('result').innerHTML = inp1 + " + " + inp2 + " = " + plus;
+};
+
+function toMinus(){
+    document.getElementById('result').innerHTML = inp1 + " - " + inp2 + " = " + minus;
+};
+
+function toMult(){
+    document.getElementById('result').innerHTML = inp1 + " X " + inp2 + " = " + mult;
+};
+
+function toDiv(){
+    document.getElementById('result').innerHTML = inp1 + " / " + inp2 + " = " + div;
+};
 
 $(".btn1").click(function(){
     $("#result").text(inp1 + " + " + inp2 + " = " + plus);
