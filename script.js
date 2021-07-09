358ce06a0bd951fd3adc8e31b2d0792c527f5828
Amimul ihsan <amimulit21@gmail.com>
2021-07-09 22:19:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,37 +7,34 @@ var mult = inp1*inp2;
 var div = inp1/inp2;
 */
 
-$("#result").text();
+/*$("#result").text();*/
+
+
 
 $(".btn2").click(function(){
-	var inp1 = $(".input1").val();
-	var inp2 = $(".input2").val();
+	var inp1 = document.querySelector("#input1").value;
+	var inp2 = document.querySelector("#input2").value;
 	var minus = inp1-inp2;
-
     $("#result").text(inp1 + " - " + inp2 + " = " + minus);
 });
 
 $(".btn1").click(function(){
-	var inp1 = $(".input1").val();
-	var inp2 = $(".input2").val();
-
+	var inp1 = $("#input1").val();
+	var inp2 = $("#input2").val();
 	var plus = parseInt(inp1)+parseInt(inp2);
-
     $("#result").text(inp1 + " + " + inp2 + " = " + plus);
 });
 
 $(".btn3").click(function(){
-	var inp1 = $(".input1").val();
-	var inp2 = $(".input2").val();
-
+	var inp1 = $("#input1").val();
+	var inp2 = $("#input2").val();
 	var mult = inp1*inp2;
-
     $("#result").text(inp1 + " X " + inp2 + " = " + mult);
 });
 
 $(".btn4").click(function(){
-	var inp1 = $(".input1").val();
-	var inp2 = $(".input2").val();
+	var inp1 = $("#input1").val();
+	var inp2 = $("#input2").val();
 	var div = inp1/inp2;
     $("#result").text(inp1 + " / " + inp2 + " = " + div);
 });
